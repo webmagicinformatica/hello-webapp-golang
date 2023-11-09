9158743f8668e95c305f7d693df55b0be9a39a0e
Dabeer Shaikh <dab8106@gmail.com>
2023-11-09 14:50:08 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
+// main_test.go
 package main
 
 import (
-    "net/http"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello, World!"))
-}
+func TestHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handler)
+
+	handler.ServeHTTP(rr, req)
+
+	expected := "Hello, World!"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
 }
